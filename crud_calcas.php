6059b5fc2e020bcfbef7e3bd7930ceb216d57dae
Marcos-2021319354 <145121811+Marcos-2021319354@users.noreply.github.com>
2023-12-07 20:00:27 -0300

    Add files via upload

--- crud_calcas.php
+++ crud_calcas.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+
+// Verifica se o usuário está autenticado
+if (!isset($_SESSION['email'])) {
+    header('Location: index.php');
+    exit();
+}
+
+// Se o usuário estiver autenticado, exibe a página protegida
+$email = $_SESSION['email'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +19,9 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+  <?php require_once "navbar.php" ?>
   <div class="container mt-5">
+  
     <h1 class="text-center">CRUD de Calças</h1>
 
     <form id="form" class="mb-4">
--- crud_camisetas.php
+++ crud_camisetas.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+
+// Verifica se o usuário está autenticado
+if (!isset($_SESSION['email'])) {
+    header('Location: index.php');
+    exit();
+}
+
+// Se o usuário estiver autenticado, exibe a página protegida
+$email = $_SESSION['email'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +19,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+<?php require_once "navbar.php" ?>
   <div class="container mt-5">
     <h1 class="text-center">CRUD de Camisetas</h1>
 
