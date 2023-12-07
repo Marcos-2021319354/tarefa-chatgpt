2565b9089d0b9e3a496ce54d6aeefb4c1dc04a77
orelhudo0369 <136131319+orelhudo0369@users.noreply.github.com>
2023-12-07 20:43:14 -0300

    Add files via upload

---
+++ tarefa-chatgpt-main/adicionar_calca.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+// Verifica se o usuário está autenticado
+if (!isset($_SESSION['email'])) {
+    header('Location: index.php');
+    exit();
+}
+
+// Se o usuário estiver autenticado, exibe a página protegida
+$email = $_SESSION['email'];
+
+// Conexão com o banco de dados (substitua as credenciais conforme necessário)
+$host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'crud_chat';
+
+$conn = new mysqli($host, $db_user, $db_pass, $db_name);
+
+if ($conn->connect_error) {
+    die("Erro de conexão: " . $conn->connect_error);
+}
+
+// Verifica se os dados foram recebidos corretamente
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nome = $_POST['nome'];
+    $tamanho = $_POST['tamanho'];
+    $cor = $_POST['cor'];
+
+    // Insere a nova Calça na tabela do banco de dados
+    $sql = "INSERT INTO calcas (nome, tamanho, cor) VALUES ('$nome', '$tamanho', '$cor')";
+    if ($conn->query($sql) === TRUE) {
+        echo "Calça adicionada com sucesso!";
+    } else {
+        echo "Erro ao adicionar a calça: " . $conn->error;
+    }
+}
+
+
+?>
